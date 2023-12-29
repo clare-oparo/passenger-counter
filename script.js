5d9313f96e5c10e47cf4e88ab6a9a274bc7e07ad
Clare <clare.oparo@student.moringaschool.com>
2023-12-29 12:27:21 +0300

    updated js with functions

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// document.getElementById("counter-el").innerText = 0
 
+
+//initialize count as 0
+//listen for clicks on increment button
+//increment the count variable when button is clicked
+//change the countel in html to reflect the new count
+
+let countEl = document.getElementById("counter-el")
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count);
+    
+}
+
+function save(){
+    console.log(count)
+}
 
 
 
